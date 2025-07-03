4a97c75ee4242cf63001baeaac586361a5cd941b
Karthik-S-Salian <karthikssalian5@gmail.com>
2025-07-03 10:14:47 +0530

    few refactor

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,8 +1,8 @@
 export const contacts = {
-    email:"karthikssalian5@gmail.com",
-    linkedin:"https://www.linkedin.com/in/karthik-s-salian/",
-    instagram:"https://www.instagram.com/karthik_s_.salian/",
-    twitter:"https://x.com/karthikSSalian",
-    github:"https://github.com/Karthik-S-Salian",
-    resume:""
+    email: "karthikssalian5@gmail.com",
+    linkedin: "https://www.linkedin.com/in/karthik-s-salian/",
+    instagram: "https://www.instagram.com/karthik_s_.salian/",
+    twitter: "https://x.com/karthikSSalian",
+    github: "https://github.com/Karthik-S-Salian",
+    resume: ""
 }
