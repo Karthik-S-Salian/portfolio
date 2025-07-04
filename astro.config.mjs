5b1c287eca6cfa1a32d2cc1af1fd252918d102af
Karthik-S-Salian <karthikssalian5@gmail.com>
2025-07-04 18:15:31 +0530

    opengraph updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://karthik-s-salian.vercel.app",
   vite: {
     plugins: [tailwindcss()]
   }
